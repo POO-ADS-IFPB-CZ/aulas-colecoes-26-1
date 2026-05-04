309a7f4f96970f397d74a87c7f17dd75aeb9101b
Paulo Freitas <paulo.freitas.nt@gmail.com>
2026-05-04 09:28:15 -0300

    exemplo com fila

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,19 @@
+import model.Fila;
 import model.Produto;
 import model.Supermercado;
 
 void main() {
 
-    Supermercado supermercado = new Supermercado("123",
-            "Cajazeiras");
-    supermercado.createProduto(new Produto(1,"Arroz",
-            4.5f));
-    //Não vai - retorna false....
-    supermercado.createProduto(new Produto(1,"Arroz",
-            4.5f));
-    supermercado.createProduto(new Produto(2, "Feijão",
-            8));
-    supermercado.createProduto(new Produto(3, "Macarrão",
-            3));
-    supermercado.createProduto(new Produto(4,"Café",
-            14));
-    supermercado.atualizar(new Produto(4, "Margarina",
-            7));
-    System.out.println(supermercado.readProdutos());
+    //TODO: fazer o menu para esse programa
+    //TODO: Adicionar prioridades
+    Fila fila = new Fila();
+    System.out.println("Nova ficha: "+fila.novaFicha());
+    System.out.println("Nova ficha: "+fila.novaFicha());
+    System.out.println("Nova ficha: "+fila.novaFicha());
+    System.out.println("Nova ficha: "+fila.novaFicha());
+
+    System.out.println("Ver próximo "+fila.verificarProximo());
+    System.out.println("Chamar: "+fila.chamarProximo());
+    System.out.println("Ver próximo "+fila.verificarProximo());
 
 }
