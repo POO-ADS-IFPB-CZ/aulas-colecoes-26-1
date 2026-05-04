383bc3cdc69700ca75ccd639c4115762b857ced8
Paulo Freitas <paulo.freitas.nt@gmail.com>
2026-05-04 09:21:01 -0300

    adicionando exemplo de fila

---
+++ src/model/Ficha.java
@@ -0,0 +1,39 @@
+package model;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class Ficha {
+
+    private UUID identificador;
+    private LocalDateTime dataHora;
+
+    public Ficha(){
+        identificador = UUID.randomUUID();
+        dataHora = LocalDateTime.now();
+    }
+
+    public UUID getIdentificador() {
+        return identificador;
+    }
+
+    public void setIdentificador(UUID identificador) {
+        this.identificador = identificador;
+    }
+
+    public LocalDateTime getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(LocalDateTime dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    @Override
+    public String toString() {
+        return "Ficha{" +
+                "identificador=" + identificador +
+                ", dataHora=" + dataHora +
+                '}';
+    }
+}
