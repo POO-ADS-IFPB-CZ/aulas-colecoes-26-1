1973f3a06566059017e249a57b6e457f94783e4d
Paulo Freitas <paulo.freitas.nt@gmail.com>
2026-05-06 07:54:55 -0300

    Exemplos de uso de collections

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,29 @@
-import model.Fila;
-import model.Produto;
-import model.Supermercado;
-
 void main() {
 
-    //TODO: fazer o menu para esse programa
-    //TODO: Adicionar prioridades
-    Fila fila = new Fila();
-    System.out.println("Nova ficha: "+fila.novaFicha());
-    System.out.println("Nova ficha: "+fila.novaFicha());
-    System.out.println("Nova ficha: "+fila.novaFicha());
-    System.out.println("Nova ficha: "+fila.novaFicha());
+    List<String> lista = new ArrayList<>();
+    lista.add("João");
+    lista.add("Maria");
+    lista.add("José");
+    lista.add("Ana");
 
-    System.out.println("Ver próximo "+fila.verificarProximo());
-    System.out.println("Chamar: "+fila.chamarProximo());
-    System.out.println("Ver próximo "+fila.verificarProximo());
+    Collections.sort(lista);
+    System.out.println("Lista ordenada: "+lista);
+
+    System.out.println(Collections.binarySearch(lista, "Joaquim"));
+
+    Collections.reverse(lista);
+    System.out.println("Ordem inversa: "+lista);
+
+    System.out.println("Máximo: "+Collections.max(lista));
+    System.out.println("Mínimo: "+Collections.min(lista));
+    Collections.shuffle(lista);
+    System.out.println("Embaralhar: "+lista);
+    Collections.swap(lista, 0, 2);
+    System.out.println(lista);
+
+    List<String> naoModificada = Collections.unmodifiableList(lista);
+    System.out.println(naoModificada);
+    naoModificada.add("Qualquer coisa");
+
 
 }
